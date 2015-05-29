13e75584055e08cf9804b7d51f57152f85fc3a39
Andrea <andrea.zorzi.94@gmail.com>
2015-05-29 18:50:51 +0200

    Doing syncing...

---
+++ src/main/webapp/js/controller/plays.js
@@ -0,0 +1,19 @@
+(function() {
+    "use strict";
+
+    angular.module('PlaysModule', []).controller('ArchiveController', function () {
+        this.archive = [
+            {
+                title : "Questo è il titolo 1",
+                title2 : "Questo è il titolo 2",
+                playbill : "img/temporary/images.jpg",
+                description : "Questa è una descrizione",
+            },{
+                title : "Questo è il asdf 1",
+                title2 : "Questo è il asf 2",
+                playbill : "img/temporary/images.jpg",
+                description : "Questa è asgsdasd descrizione",
+            }
+        ];
+    });
+})();
