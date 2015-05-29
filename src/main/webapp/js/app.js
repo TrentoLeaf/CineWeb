420c4f2347c63420f4456ebfc5bd809cdffa25d5
Stiven <ekstefo@gmail.com>
2015-05-29 18:54:08 +0200

    add navigation right-panel

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-  $(".button-collapse").sideNav();
-});
+(function() {
+  'use strict';
+
+  var app= angular.module('Main', ['PlaysModule']);
+  app.controller('TabController', function() {
+    this.tab = 1;
+    this.setTab = function (tab) {
+      this.tab = tab;
+    };
+    this.isSet = function (Value) {
+      return this.tab === Value;
+    };
+  });
+})();
