cb3027e2758e65f1189e428c72b3c1a0bdff71d2
Willy <rizzi.williams@me.com>
2015-05-29 17:18:22 +0200

    Added first repeat function

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-  $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav();
 });
+
+(function() {
+    var app = angular.module('playbillcontainer', []);
+    app.controller('ArchiveController', function () {
+        this.archive = [
+            {
+            title : "Questo è il titolo 1",
+            title2 : "Questo è il titolo 2",
+            playbill : "img/temporary/images.jpg",
+            description : "Questa è una descrizione",
+            },{
+            title : "Questo è il asdf 1",
+            title2 : "Questo è il asf 2",
+            playbill : "img/temporary/images.jpg",
+            description : "Questa è asgsdasd descrizione",
+            }
+        ];
+    });
+})();
