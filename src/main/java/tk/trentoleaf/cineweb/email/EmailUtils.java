80b1c1df68556fa3dc09aefa60fca9214262d4d2
Stiven <ekstefo@gmail.com>
2015-05-30 22:55:18 +0200

    updating email texts

--- src/main/java/tk/trentoleaf/cineweb/email/EmailUtils.java
+++ src/main/java/tk/trentoleaf/cineweb/email/EmailUtils.java
@@ -5,13 +5,23 @@ package tk.trentoleaf.cineweb.email;
  */
 public class EmailUtils {
 
-    public static String registrationText (String firstName, String secondName, String url){
-        StringBuilder sb =  new StringBuilder();
+    public static String registrationText(String firstName, String secondName, String url) {
+        StringBuilder sb = new StringBuilder();
 
-                sb.append("Gentile " + firstName + " " + secondName + ",\n " +
-                "La Sua richiesta di iscrizione è stata inoltrata con successo." +
-                "Per completare la Sua iscrizione prema questo link:\n\n" + url + "\n\n"
-                );
+        sb.append("Gentile " + firstName + " " + secondName + ",\n " +
+                        "La Sua richiesta di iscrizione è stata inoltrata con successo." +
+                        " Per completare la Sua iscrizione prema questo link:\n\n" + url + "\n\n"
+        );
+
+        return sb.toString();
+    }
+
+
+    public static String resetPasswordText(String url) {
+        StringBuilder sb =new StringBuilder();
+
+        sb.append("Come da sua richiesta le è stata mandata la possibilità di ripristinare la sua password.\n" +
+                " Per completare questa operazione clicchi sul link sottostante.\n\n" + url + "\n\n ");
 
         return sb.toString();
     }
