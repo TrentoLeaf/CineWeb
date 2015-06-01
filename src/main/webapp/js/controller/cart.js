b72a754ffe122ac7fcbc4dd49aa3343f7e1c8b77
Andrea <andrea.zorzi.94@gmail.com>
2015-06-01 12:48:59 +0200

    Ora il carrello praticamente funziona.

--- src/main/webapp/js/controller/cart.js
+++ src/main/webapp/js/controller/cart.js
@@ -3,85 +3,74 @@
 
     angular.module('CartModule', []).controller('CartController', ['$rootScope', function ($rootScope) {
 
-        this.prices = {
-            ticket_normale: 8.50,
-            ticket_ridotto: 5.50,
-            ticket_militare: 6,
-            ticket_disabile: 7
-        };
-
-        $rootScope.ticket_types = [{name: "Normale"},{name:'Ridotto'},{name:'Militari'},{name:'Disabili'}];
+        // TODO dati da recuperare dal server
+        $rootScope.tickets = [
+            {type: "normale", name:"Normale", price: 8.50},
+            {type: "ridotto", name:"Ridotto", price: 5.50},
+            {type: "militare", name:"Militare", price: 6},
+            {type: "disabile", name:"Disabile", price: 7}
+        ];
 
         this.maxTickets = 10;
 
         // carrello che contiene oggetti film modificati
-        $rootScope.cart = [{
-            title: "Titolo 1",
-            date: "Data 1",
-            time: "Orario 1",
-            gender: "Genere 1",
-            playbill: "img/temporary/img1.jpg",
-            description: "Descrizione 1",
-            dropdowns: [1, 1],
-            normale: 2,
-            ridotto: 0,
-            militari: 0,
-            disabili: 0
-        }];
+        $rootScope.cart = [];
 
         $rootScope.total = 0.00;
 
         this.addToCart = function (film) {
-            // numero di ticket per tipologia
-            film['normale'] = 1;
-            film['ridotto'] = 0;
-            film['militari'] = 0;
-            film['disabili'] = 0;
-            film['dropdowns'] = [1];
-            $rootScope.cart.push(film);
-            this.updateTotal();
+            var duplicate = false;
+
+            // controllo di non inserire dublicati
+            for (var i = 0; i< $rootScope.cart.length; i++) {
+                // TODO assicurarsi che film abbia un fid
+                if (film.fid == $rootScope.cart[i].fid) {
+                    duplicate = true;
+                    i = $rootScope.cart.length;
+                }
+            }
+
+            if (! duplicate) {
+                // aggiungo un dropdown con tipo e numero di biglietti
+                film['tickets'] = [];
+                film['tickets'].push({ type : $rootScope.tickets[0].type, number : 1});
+
+                $rootScope.cart.push(film);
+                this.updateTotal();
+            }
         };
 
         this.removeFromCart = function (film) {
-            $rootScope.cart.splice(film);
+            $rootScope.cart.splice(film, 1); // rimuove 1 elemento a partire da 'film'
             this.updateTotal();
         };
 
-        this.updateTiket = function (film, type, number) {
-            if (number == 0) {
-                this.removeDropDown(film);
-            } else {
-                $rootScope.cart[film][type] = number;
-            }
-            this.updateTotal();
-        };
-
+        // aggiunge un nuovo selettore per tipo e numero di biglietti
         this.addDropDown = function (film) {
-            if (($rootScope.cart[film]['dropdowns']).length < 4) {
-                $rootScope.cart[film]['dropdowns'].push(1);
+            if (($rootScope.cart[film].tickets.length) < 4) {
+                $rootScope.cart[film].tickets.push({ type : $rootScope.tickets[0].type, number : 1});
             }
-            console.log($rootScope.cart[film]);
+            this.updateTotal();
         };
 
-        this.removeDropDown = function (film) {
-            if ($rootScope.cart[film]['dropdowns'].length == 1) {
-                this.removeFromCart(film);
-            }
-            else {
-                $rootScope.cart[film]['dropdowns'].push(1);
-            }
-        };
-
+        /* aggiorna il totale controllando per tutti il film nel carrello,
+        il numero e tipo di biglietti e li moltiplica per il  loro prezzo
+        */
         this.updateTotal = function () {
             $rootScope.total = 0;
 
             for (var i = 0; i < $rootScope.cart.length; i++) {
-                $rootScope.total = $rootScope.total
-                    + ((this.prices['ticket_normale'] * $rootScope.cart[i]['normale'])
-                    + (this.prices['ticket_ridotto'] * $rootScope.cart[i]['ridotto'])
-                    + (this.prices['ticket_militare'] * $rootScope.cart[i]['militari'])
-                    + (this.prices['ticket_disabile'] * $rootScope.cart[i]['disabili']));
+                for (var j = 0; j < $rootScope.tickets.length; j++) {
+                    var num = 0;
+                    for (var k = 0; k < $rootScope.cart[i].tickets.length; k++) {
+                        if ($rootScope.cart[i].tickets[k].type == $rootScope.tickets[j].type) {
+                            num = num + $rootScope.cart[i].tickets[k].number;
+                        }
+                    }
+                    $rootScope.total = $rootScope.total + ($rootScope.tickets[j]['price'] * num);
+                }
             }
+
         };
 
     }]);
