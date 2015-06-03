0cfccb0c19e5fbdb7554aa1a57fff211e4aebbc9
Davide Pedranz <davide.pedranz@gmail.com>
2015-06-03 22:19:17 +0200

    Logger filter

---
+++ src/main/java/tk/trentoleaf/cineweb/rest/filters/LoggerFilter.java
@@ -0,0 +1,37 @@
+package tk.trentoleaf.cineweb.rest.filters;
+
+import tk.trentoleaf.cineweb.model.User;
+
+import javax.annotation.Priority;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Logger;
+
+@Provider
+@Priority(1)
+@PreMatching
+public class LoggerFilter implements ContainerRequestFilter {
+    private Logger logger = Logger.getLogger(LoggerFilter.class.getSimpleName());
+
+    @Context
+    private HttpServletRequest httpRequest;
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        // check if a logged user
+        final HttpSession session = httpRequest.getSession(false);
+        final User user = (session != null) ? (User) session.getAttribute("user") : null;
+        final Integer uid = (user != null) ? user.getUid() : null;
+
+        // log the request
+        logger.info("REQUEST at " + new Date() + " [uid = " + uid + "]: " + requestContext.getUriInfo().getRequestUri());
+    }
+}
