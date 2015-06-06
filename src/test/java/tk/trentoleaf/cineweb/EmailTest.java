8d8b376620cbc76887193eaabe2fd10d4e3ad57b
Davide Pedranz <davide.pedranz@gmail.com>
2015-06-06 10:23:56 +0200

    Fix tests

--- src/test/java/tk/trentoleaf/cineweb/EmailTest.java
+++ src/test/java/tk/trentoleaf/cineweb/EmailTest.java
@@ -1,35 +1,27 @@
 package tk.trentoleaf.cineweb;
 
 import org.junit.Test;
-import com.sendgrid.SendGridException;
 import tk.trentoleaf.cineweb.email.EmailSender;
-import tk.trentoleaf.cineweb.email.EmailUtils;
 import tk.trentoleaf.cineweb.email.pdf.FilmTicketData;
+import tk.trentoleaf.cineweb.model.Role;
 import tk.trentoleaf.cineweb.model.User;
 
-import java.io.IOException;
 import java.net.URI;
 
 public class EmailTest {
 
     @Test
     public void test() {
-        String s = EmailUtils.registrationText("nome", "cognome", "url");
-        String c = EmailUtils.registrationText("Williams", "Rizzi", "www.williams.it");
-        String t = EmailUtils.registrationText("Trento", "Leaf+", "www.trentoleaf.tk");
 
+        final String EMAIL = "email@email.com";
 
         try {
-            EmailSender.sendTicketPDFEmail(URI.create("aaa"),new User(),new FilmTicketData());
-        } catch (SendGridException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            final User u = new User(true, Role.CLIENT, EMAIL, "pass", "name", "name");
+            EmailSender.sendTicketPDFEmail(URI.create("aaa"), u, new FilmTicketData());
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-
-        int a=2;
     }
-
 
 }
