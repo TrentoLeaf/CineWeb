4ed20ccd68835452bc3c504378da7bd2fc413af6
Davide Pedranz <davide.pedranz@gmail.com>
2015-06-17 00:56:33 +0200

    End test plays rest

--- src/test/java/tk/trentoleaf/cineweb/rest/RestPlayTest.java
+++ src/test/java/tk/trentoleaf/cineweb/rest/RestPlayTest.java
@@ -2,6 +2,7 @@ package tk.trentoleaf.cineweb.rest;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
+import tk.trentoleaf.cineweb.exceptions.db.EntryNotFoundException;
 import tk.trentoleaf.cineweb.model.Film;
 import tk.trentoleaf.cineweb.model.Play;
 import tk.trentoleaf.cineweb.model.Room;
@@ -168,125 +169,72 @@ public class RestPlayTest extends MyJerseyTest {
         assertEquals(409, r1.getStatus());
     }
 
-//    @Test
-//    public void editFilmSuccess() throws Exception {
-//
-//        // create film in db
-//        final Film f1 = new Film("sdofijoisdf", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
-//        db.createFilm(f1);
-//        final Film f2 = new Film("Davide amicone", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
-//
-//        // login as admin
-//        final Cookie c = loginAdmin();
-//
-//        // update user
-//        final Response r1 = getTarget().path("/films/" + f1.getFid()).request(JSON).cookie(c).put(Entity.json(f2));
-//        assertEquals(200, r1.getStatus());
-//
-//        f2.setFid(f1.getFid());
-//        assertEquals(db.getFilm(f1.getFid()), f2);
-//    }
-//
-//    @Test
-//    public void editFilmFail1() throws Exception {
-//
-//        // new film
-//        final Film f2 = new Film("Davide amicone", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
-//
-//        // login as client
-//        final Cookie c = loginClient();
-//
-//        // update user
-//        final Response r1 = getTarget().path("/films/" + 343).request(JSON).cookie(c).put(Entity.json(f2));
-//        assertEquals(401, r1.getStatus());
-//    }
-//
-//    @Test
-//    public void editFilmFail2() throws Exception {
-//
-//        // new film
-//        final Film f2 = new Film("Davide amicone", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
-//
-//        // no login
-//
-//        // update user
-//        final Response r1 = getTarget().path("/films/" + 343).request(JSON).put(Entity.json(f2));
-//        assertEquals(401, r1.getStatus());
-//    }
-//
-//    @Test
-//    public void editFilmFail3() throws Exception {
-//
-//        // create film in db
-//        final Film f1 = new Film("sdofijoisdf", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
-//        db.createFilm(f1);
-//        final Film f2 = new Film("Davide amicone", "sfdsf", "not an url", "http://aaaa.org", "trama moltooo lunga", 120);
-//
-//        // login as admin
-//        final Cookie c = loginAdmin();
-//
-//        // update user
-//        final Response r1 = getTarget().path("/films/" + f1.getFid()).request(JSON).cookie(c).put(Entity.json(f2));
-//        assertEquals(400, r1.getStatus());
-//    }
-//
-//    @Test(expected = EntryNotFoundException.class)
-//    public void deleteFilmSuccess() throws Exception {
-//
-//        // create film to delete in db
-//        final Film f1 = new Film("sdofijoisdf", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
-//        db.createFilm(f1);
-//
-//        // login as admin
-//        final Cookie c = loginAdmin();
-//
-//        // delete user
-//        final Response r1 = getTarget().path("/films/" + f1.getFid()).request(JSON).cookie(c).delete();
-//        assertEquals(200, r1.getStatus());
-//
-//        // should throw an exception
-//        db.getFilm(f1.getFid());
-//    }
-//
-//    @Test
-//    public void deleteFilmFail1() throws Exception {
-//
-//        // login as admin
-//        final Cookie c = loginAdmin();
-//
-//        // delete user
-//        final Response r1 = getTarget().path("/films/" + 234).request(JSON).cookie(c).delete();
-//        assertEquals(404, r1.getStatus());
-//    }
-//
-//    @Test
-//    public void deleteFilmFail2() throws Exception {
-//
-//        // create film to delete in db
-//        final Film f1 = new Film("sdofijoisdf", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
-//        db.createFilm(f1);
-//
-//        // login as client
-//        final Cookie c = loginClient();
-//
-//        // delete user
-//        final Response r1 = getTarget().path("/films/" + f1.getFid()).request(JSON).cookie(c).delete();
-//        assertEquals(401, r1.getStatus());
-//    }
-//
-//    @Test
-//    public void deleteFilmFail3() throws Exception {
-//
-//        // create film to delete in db
-//        final Film f1 = new Film("sdofijoisdf", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
-//        db.createFilm(f1);
-//
-//        // no login
-//
-//        // delete user
-//        final Response r1 = getTarget().path("/films/" + f1.getFid()).request(JSON).delete();
-//        assertEquals(401, r1.getStatus());
-//    }
+    @Test(expected = EntryNotFoundException.class)
+    public void deletePlaySuccess() throws Exception {
+
+        // film and room and play
+        final Film f1 = new Film("Teo alla ricerca della pizza perduta", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
+        db.createFilm(f1);
+        final Room room = db.createRoom(3, 4);
+        final Play p1 = new Play(f1, room, DateTime.now(), true);
+        db.createPlay(p1);
+
+        // login as admin
+        final Cookie c = loginAdmin();
+
+        // delete user
+        final Response r1 = getTarget().path("/plays/" + p1.getPid()).request(JSON).cookie(c).delete();
+        assertEquals(200, r1.getStatus());
+
+        // should throw an exception
+        db.getPlay(p1.getPid());
+    }
+
+    @Test
+    public void deletePlayFail1() throws Exception {
+
+        // login as admin
+        final Cookie c = loginAdmin();
+
+        // delete user
+        final Response r1 = getTarget().path("/plays/" + 234).request(JSON).cookie(c).delete();
+        assertEquals(404, r1.getStatus());
+    }
+
+    @Test
+    public void deletePlayFail2() throws Exception {
+
+        // film and room and play
+        final Film f1 = new Film("Teo alla ricerca della pizza perduta", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
+        db.createFilm(f1);
+        final Room room = db.createRoom(3, 4);
+        final Play p1 = new Play(f1, room, DateTime.now(), true);
+        db.createPlay(p1);
+
+        // login as client
+        final Cookie c = loginClient();
+
+        // delete user
+        final Response r1 = getTarget().path("/plays/" + p1.getPid()).request(JSON).cookie(c).delete();
+        assertEquals(401, r1.getStatus());
+    }
+
+    @Test
+    public void deletePlayFail3() throws Exception {
+
+        // film and room and play
+        final Film f1 = new Film("Teo alla ricerca della pizza perduta", "fantasy", "http://aaa.com", "http://aaaa.org", "trama moltooo lunga", 120);
+        db.createFilm(f1);
+        final Room room = db.createRoom(3, 4);
+        final Play p1 = new Play(f1, room, DateTime.now(), true);
+        db.createPlay(p1);
+
+        // no login
+
+        // delete user
+        final Response r1 = getTarget().path("/films/" + p1.getPid()).request(JSON).delete();
+        assertEquals(401, r1.getStatus());
+    }
 
 }
 
