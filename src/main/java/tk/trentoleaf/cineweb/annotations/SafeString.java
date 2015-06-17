a1ea6492ba3c76965bc44ba28d26cd818443e2b6
Davide Pedranz <davide.pedranz@gmail.com>
2015-06-18 00:42:59 +0200

    Fix XSS annotation

--- src/main/java/tk/trentoleaf/cineweb/annotations/SafeString.java
+++ src/main/java/tk/trentoleaf/cineweb/annotations/SafeString.java
@@ -1,28 +1,30 @@
 package tk.trentoleaf.cineweb.annotations;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.SafeHtml;
-
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.*;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.constraints.NotNull;
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 @NotNull
-@SafeHtml
+@NotEmpty
+@SafeHtml(whitelistType = SafeHtml.WhiteListType.NONE)
 @Target({METHOD, FIELD, ANNOTATION_TYPE})
 @Retention(RUNTIME)
 @Constraint(validatedBy = {})
-@Documented
 public @interface SafeString {
 
-    String message() default "The String cannot be null nor contain XML tags";
+    String message() default "The String cannot be null nor contain HTML tags";
 
+    @SuppressWarnings("unused")
     Class<?>[] groups() default {};
 
+    @SuppressWarnings("unused")
     Class<? extends Payload>[] payload() default {};
 }
