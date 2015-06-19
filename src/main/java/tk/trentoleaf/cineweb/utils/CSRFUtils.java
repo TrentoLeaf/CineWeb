4edee73842978f5cfa0dd6980bed16a3b9ec4c86
Davide Pedranz <davide.pedranz@gmail.com>
2015-06-19 19:54:41 +0200

    Document CSRF utils

--- src/main/java/tk/trentoleaf/cineweb/utils/CSRFUtils.java
+++ src/main/java/tk/trentoleaf/cineweb/utils/CSRFUtils.java
@@ -7,24 +7,35 @@ import javax.servlet.http.HttpSession;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
+/**
+ * This class contains some utilities used to set a random CSRF token and store it in the current session
+ * for the following checks.
+ *
+ * @see tk.trentoleaf.cineweb.filters.all.CSRFFilter
+ */
 public class CSRFUtils {
 
+    // constants
     public static final String COOKIE = "XSRF-TOKEN";
     public static final String HEADER = "X-XSRF-TOKEN";
     public static final String SESSION = "csrf";
 
+    // random number generator, used to generate the token value
     private static SecureRandom random = new SecureRandom();
 
+    // generate a random string
     public static String randomString() {
         return new BigInteger(150, random).toString(32);
     }
 
+    // generate and store a random CSRF token on new sessions
     public static void addCSRFToken(HttpServletRequest request, HttpServletResponse response) {
 
         // create session if not exists
         final HttpSession session = request.getSession(true);
 
         // if session new -> add csrf cookie
+        // if session already exists -> the token should be already present
         if (session.isNew()) {
 
             // generate CSRF nonce...
@@ -35,14 +46,15 @@ public class CSRFUtils {
             csrfCookie.setPath("/");
 
             // check if request over https
+            // and secure the cookie
             if (request.isSecure()) {
                 csrfCookie.setSecure(true);
             }
 
-            // add cookie
+            // add the cookie to the response
             response.addCookie(csrfCookie);
 
-            // save value in session
+            // save the token value in the current session
             session.setAttribute(CSRFUtils.SESSION, csrfValue);
         }
     }
