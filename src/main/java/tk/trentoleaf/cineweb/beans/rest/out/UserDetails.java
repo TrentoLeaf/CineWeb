cc78ac59e912d015090eda34dc474242e773dc4b
Davide Pedranz <davide.pedranz@gmail.com>
2015-06-22 12:26:33 +0200

    Add role to /me

--- src/main/java/tk/trentoleaf/cineweb/beans/rest/out/UserDetails.java
+++ src/main/java/tk/trentoleaf/cineweb/beans/rest/out/UserDetails.java
@@ -1,5 +1,6 @@
 package tk.trentoleaf.cineweb.beans.rest.out;
 
+import tk.trentoleaf.cineweb.beans.model.Role;
 import tk.trentoleaf.cineweb.beans.model.User;
 
 import java.io.Serializable;
@@ -10,12 +11,14 @@ public class UserDetails implements Serializable {
     private String firstName;
     private String secondName;
     private double credit;
+    private Role role;
 
     public UserDetails(User user) {
         this.email = user.getEmail();
         this.firstName = user.getFirstName();
         this.secondName = user.getSecondName();
         this.credit = user.getCredit();
+        this.role = user.getRole();
     }
 
 }
