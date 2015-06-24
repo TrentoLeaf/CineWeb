5d851ce793f1416eb4a50fc05d10e7c89d311409
samdja <samuelgiacomelli@gmail.com>
2015-06-24 17:09:18 +0200

    La navigazione in mare aperto per gli admin e' piu' facile che per gli utenti normali, mondo crudele.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -80,6 +80,10 @@ $(document).ready(function () {
                 templateUrl: '../partials/admin/films.html',
                 controller: 'AdminFilmsController',
                 controllerAs: 'ctrl'
+            }).when ('/admin/films/new',{
+                templateUrl: '../partials/admin/new_film.html',
+                controller: 'AdminFilmsController',
+                controllerAs: 'ctrl'
             }).when ('/admin/plays',{
                 templateUrl: '../partials/admin/plays.html'
             }).when ('/admin/prices',{
@@ -96,7 +100,7 @@ $(document).ready(function () {
                 templateUrl: '../partials/admin/users.html',
                 controller: 'AdminUsersController',
                 controllerAs: 'ctrl'
-            }).when('/admin/users/new_user', {
+            }).when('/admin/users/new', {
                 templateUrl: '../partials/admin/new_user.html',
                 controller: 'AdminUsersController',
                 controllerAs: 'ctrl'
