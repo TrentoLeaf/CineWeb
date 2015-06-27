67393939d9400a87a8a63ff2ce809694e2e90617
samdja <samuelgiacomelli@gmail.com>
2015-06-27 14:43:30 +0200

    Double password check during registration

--- src/main/webapp/js/controller/registration.js
+++ src/main/webapp/js/controller/registration.js
@@ -8,6 +8,7 @@
 
             this.status = "";
             this.user = {};
+            this.verifyPassword = "";
 
             var setStatus = function (msg) {
                 ctrl.status = msg;
@@ -21,28 +22,40 @@
                 $('#registration_message').addClass("white-text");
                 setStatus("Un momento...");
 
-                Auth.registration(user.email, user.password, user.firstName, user.secondName)
-                    .success(
-                    function (data) {
-                        $('#registration_message').removeClass("red-text white-text");
-                        $('#registration_message').addClass("green-text");
-                        setStatus("La richiesta è stata inoltrata. Controlla la tua casella di posta.");
-                        console.log(data);
-                        $('.registration-loader').removeClass('active');
-                    })
-                    .error(
-                    function (error) {
-                        $('#registration_message').removeClass("green-text white-text");
-                        $('#registration_message').addClass("red-text");
-                        if (error.error[0] == ".") {
-                            setStatus("La mail è già stata utilizzata. Si prega di cambiarla.");
-                        } else {
-                            setStatus("La richiesta non è andata a buon fine. Ricontrolla i dati inseriti.");
+                if (ctrl.verifyPassword == ctrl.user.password) {
+                    Auth.registration(user.email, user.password, user.firstName, user.secondName)
+                        .success(
+                        function (data) {
+                            $('#registration_message').removeClass("red-text white-text");
+                            $('#registration_message').addClass("green-text");
+                            setStatus("La richiesta è stata inoltrata. Controlla la tua casella di posta.");
+                            console.log(data);
+                            $('.registration-loader').removeClass('active');
+                        })
+                        .error(
+                        function (error) {
+                            $('#registration_message').removeClass("green-text white-text");
+                            $('#registration_message').addClass("red-text");
+                            if (error.error[0] == ".") {
+                                setStatus("La mail è già stata utilizzata. Si prega di cambiarla.");
+                            } else {
+                                setStatus("La richiesta non è andata a buon fine. Ricontrolla i dati inseriti.");
+                            }
+                            console.log(error);
+                            $('.registration-loader').removeClass('active');
                         }
-                        console.log(error);
-                        $('.registration-loader').removeClass('active');
-                    }
-                );
+                    );
+                } else {
+                    $('#registration_message').removeClass("green-text white-text");
+                    $('#registration_message').addClass("red-text");
+                    ctrl.user.password = "";
+                    ctrl.verifyPassword = "";
+                    $('#password').addClass("invalid");
+                    $('#password').focus();
+                    $('#verifyPassword').addClass("invalid");
+                    setStatus("Le password inserite non combaciano, riprova.");
+                    $('.registration-loader').removeClass('active');
+                };
             };
         }]);
 
