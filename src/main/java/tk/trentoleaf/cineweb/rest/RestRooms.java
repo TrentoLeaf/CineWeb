300523f35f000d79d504ea1524366015472ef201
Davide Pedranz <davide.pedranz@gmail.com>
2015-06-27 16:10:52 +0200

    Top rooms -> TODO: test

--- src/main/java/tk/trentoleaf/cineweb/rest/RestRooms.java
+++ src/main/java/tk/trentoleaf/cineweb/rest/RestRooms.java
@@ -1,11 +1,17 @@
 package tk.trentoleaf.cineweb.rest;
 
+import tk.trentoleaf.cineweb.annotations.rest.AdminArea;
 import tk.trentoleaf.cineweb.annotations.rest.Compress;
 import tk.trentoleaf.cineweb.beans.model.Room;
+import tk.trentoleaf.cineweb.beans.model.RoomStatus;
 import tk.trentoleaf.cineweb.db.RoomsDB;
+import tk.trentoleaf.cineweb.db.StatisticsDB;
+import tk.trentoleaf.cineweb.exceptions.db.EntryNotFoundException;
+import tk.trentoleaf.cineweb.exceptions.rest.NotFoundException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import java.util.List;
 
 /**
@@ -16,6 +22,7 @@ public class RestRooms {
 
     // DB singleton
     private RoomsDB roomsDB = RoomsDB.instance();
+    private StatisticsDB statisticsDB = StatisticsDB.instance();
 
     @GET
     @Compress
@@ -23,7 +30,17 @@ public class RestRooms {
         return roomsDB.getRooms(false);
     }
 
-    // TODO!
-    // posti migliori per ogni sala!
+    // TODO -> test
+    @GET
+    @Path("/{id}")
+    @AdminArea
+    @Compress
+    public RoomStatus getTopSeats(@PathParam("id") int id) {
+        try {
+            return statisticsDB.getTopSeatsByRoom(id);
+        } catch (EntryNotFoundException e) {
+            throw NotFoundException.GENERIC;
+        }
+    }
 
 }
