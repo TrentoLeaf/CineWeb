f9e669946e512b3766d5b848766fb225bf76b6e0
Davide Pedranz <davide.pedranz@gmail.com>
2015-06-28 14:13:24 +0200

    test gzip

--- src/test/java/tk/trentoleaf/cineweb/WebApp.java
+++ src/test/java/tk/trentoleaf/cineweb/WebApp.java
@@ -1,5 +1,6 @@
 package tk.trentoleaf.cineweb;
 
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -13,8 +14,8 @@ import tk.trentoleaf.cineweb.heroku.Main;
 import static org.junit.Assert.assertEquals;
 
 /**
- * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
- * command that is used for launching should fire this main method.
+ * This test launches the web application in an embedded Jetty container.
+ * Here it is possible to test WebFilters, WebListener and Servlets.
  */
 public class WebApp {
 
@@ -40,14 +41,97 @@ public class WebApp {
     @Test
     public void testCache() throws Exception {
         try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            // request
             HttpGet request = new HttpGet("http://localhost:8080/api");
             HttpResponse response = client.execute(request);
 
             // check cache header
             String cache = response.getHeaders("Cache-Control")[0].getValue();
             assertEquals("no-cache", cache);
+
+            // check pragma header
+            String pragma = response.getHeaders("Pragma")[0].getValue();
+            assertEquals("no-cache", pragma);
         }
     }
 
+    @Test
+    public void testXProtection() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            // request
+            HttpGet request = new HttpGet("http://localhost:8080/");
+            HttpResponse response = client.execute(request);
+
+            // check X-Frame-Options
+            String frame = response.getHeaders("X-Frame-Options")[0].getValue();
+            assertEquals("DENY", frame);
+
+            // check X-Content-Type-Options
+            String type = response.getHeaders("X-Content-Type-Options")[0].getValue();
+            assertEquals("nosniff", type);
+
+            // check X-XSS-Protection
+            String xss = response.getHeaders("X-XSS-Protection")[0].getValue();
+            assertEquals("1; mode=block", xss);
+        }
+    }
+
+    @Test
+    public void testGzipOnStaticFiles() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            // request
+            HttpGet request = new HttpGet("http://localhost:8080/");
+            HttpResponse response = client.execute(request);
+
+            // check gzip -> use Vary header
+            String vary = response.getHeaders("Vary")[0].getValue();
+            assertEquals("Accept-Encoding", vary);
+        }
+    }
+
+    @Test
+    public void testGzipOnApi() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            // request
+            HttpGet request = new HttpGet("http://localhost:8080/api/films");
+            HttpResponse response = client.execute(request);
+
+            // check gzip -> use Vary header
+            String vary = response.getHeaders("Vary")[0].getValue();
+            assertEquals("Accept-Encoding", vary);
+        }
+    }
+
+    @Test
+    public void testGzipOffStaticFiles() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            // request
+            HttpGet request = new HttpGet("http://localhost:8080/img/logo.png");
+            HttpResponse response = client.execute(request);
+
+            // check gzip -> use Vary header
+            Header[] vary = response.getHeaders("Vary");
+            assertEquals(0, vary.length);
+        }
+    }
+
+    @Test
+    public void testGzipOffApi() throws Exception {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            // request
+            HttpGet request = new HttpGet("http://localhost:8080/api");
+            HttpResponse response = client.execute(request);
+
+            // check gzip -> use Vary header
+            Header[] vary = response.getHeaders("Vary");
+            assertEquals(0, vary.length);
+        }
+    }
 
 }
