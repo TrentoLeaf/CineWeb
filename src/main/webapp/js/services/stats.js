37e7d62f2da3b25142cb9a058b432937c59569af
WilliamsRizzi <rizzi.williams@me.com>
2015-06-29 17:02:04 +0200

    Molti uomini hanno vita di quieta disperazione: non vi rassegnate a questo, ribellatevi, non affogatevi di pigrizia mentale, guardatevi intorno. Osate cambiare, cercate nuove strade.

---
+++ src/main/webapp/js/services/stats.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Willo on 29/06/2015.
+ */
+(function () {
+    'use strict';
+
+    angular.module('statsModule', ['constantsModule', 'ngResource'])
+
+        .factory('Stats', ['BASE', '$http', '$q', '$log', function (BASE, $http, $q, $log) {
+            var BASE_USERS = BASE + "/users";
+            var BASE_FILMS = BASE + "/films";
+
+            return {
+                topUsers: function () {
+                    return $http.get(BASE_USERS + '/top')
+                        .success(function (data) {
+                            $log.info(data);
+                        })
+                        .error(function (data, status) {
+                            $log.warn('TOP USERS NOT RETRIVED: ' + status + " " + data.error);
+                        });
+                },
+
+                grossingFilms: function () {
+                    return $http.get(BASE_FILMS + '/grossing')
+                        .success(function (data) {
+                            $log.info('GROSSING FILMS OK: ' + data);
+                        })
+                        .error(function (data, status) {
+                            $log.warn('GROSSING FILMS NOT RETRIVED: ' + status + " " + data);
+                        });
+                }
+            }
+        }]);
+
+})();
