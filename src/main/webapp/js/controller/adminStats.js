1a9c0ff1319e08a4886a165377175c8d585ea3ff
WilliamsRizzi <rizzi.williams@me.com>
2015-06-30 11:06:00 +0200

    Questo è dedicato a tutti quelli che quando dico magenta pensano ad un fuxia chiaro

--- src/main/webapp/js/controller/adminStats.js
+++ src/main/webapp/js/controller/adminStats.js
@@ -1,6 +1,4 @@
-/**
- * Created by Willo on 24/06/2015.
- */
+/*Created by Willo on 24/06/2015.*/
 (function () {
     'use strict';
 
@@ -10,26 +8,56 @@
      * -Lista dei clienti che comprano di più
      * */
 
+
+    var dataFarlocchi1 = [
+        {
+            firstName: 'asdf',
+            secondName: 'jklò',
+            spent: 12
+        },{
+            firstName: 'asdwf',
+            secondName: 'jklò',
+            spent: 12
+        },{
+            firstName: 'asdfq',
+            secondName: 'jklò',
+            spent: 12
+        },{
+            firstName: 'wasdf',
+            secondName: 'jklò',
+            spent: 12
+        }
+    ];
+
+    var dataFarlocchi2 = [
+        {
+            title: 'fuck',
+            grossing: 23
+        },{
+            title: 'fuck',
+            grossing: 23
+        },{
+            title: 'fuck',
+            grossing: 23
+        },{
+            title: 'fuck',
+            grossing: 23
+        }
+    ];
+
     angular.module('adminStats', ['statsModule'])
         .controller('AdminStatsController', ['$location', 'Films', 'Stats', function ($location, Films, Stats) {
 
             var ctrl = this;
             this.error = "";
+            var fillColors=[
+                'rgba(242,54,255,0.2)',
+                'rgba(151,187,205,0.2)'
+            ];
 
             var topClientsData = {
                 labels: [],
-                datasets: [
-                    {
-                        label: "My dataset",
-                        fillColor: "rgba(242,54,255,0.2)",
-                        strokeColor: "rgba(242,54,255,1)",
-                        pointColor: "rgba(242,54,255,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(242,54,255,1)",
-                        data: []
-                    }
-                ]
+                datasets: []
             };
 
             this.topUsers = function () {
@@ -43,16 +71,24 @@
                          "tickets": 5,
                          "spent": 35
                          */
-                        for(var user = 0; user <= data.size ; user++){
-                            topClientsData.labels.push(data[user].firstName + ' ' + data[user].secondName);
-                            console.log("name "+data[user].firstName + ' ' + data[user].secondName);
-                            topClientsData.datasets.data.push(data[user].spent);
-                            console.log("spent "+data[user].spent);
+                        for(var user in dataFarlocchi1){
+                            topClientsData.labels.push(dataFarlocchi1[user].firstName + ' ' + dataFarlocchi1[user].secondName);
+                            console.log("name "+dataFarlocchi1[user].firstName + ' ' + dataFarlocchi1[user].secondName);
+
+                            topClientsData.datasets.push(
+                                {
+                                    fillColor: fillColors[user % fillColors.size],
+                                    data : dataFarlocchi1[user].spent
+                                }
+                            );
+
+                            console.log("spent "+dataFarlocchi1[user].spent);
                         }
-                        console.log('Fine parsing dati user');
+                        console.log('Fine parsing dati user, label: ' + topClientsData.labels);
+                        console.log('dati: ' + topClientsData.datasets.data);
                     })
                     .error(function (error) {
-                        console.log('Errore in parse dati top users');
+                        console.log('Errore in dati top users');
                     });
             };
 
@@ -61,6 +97,8 @@
             var ctx = document.getElementById("topClients").getContext("2d");
             var topClients = new Chart(ctx).Bar(topClientsData, {pointDot: false});
 
+
+            /*Questo lo faccio appena capisco come popolare sto coso sopra*/
             var filmsIncomeData = {
                 labels: [],
                 datasets: [
@@ -86,11 +124,11 @@
                          "title": "Teo",
                          "grossing": 35
                          */
-                        for(var film = 0; film <= data.size; film++){
-                            filmsIncomeData.labels.push(data[film].title);
-                            console.log("title "+data[film].title);
-                            filmsIncomeData.datasets.data.push(data[film].grossing);
-                            console.log("grossing "+data[film].grossing);
+                        for(var film = 0; film <= dataFarlocchi2.size; film+=1){
+                            filmsIncomeData.labels.push(dataFarlocchi2[film].title);
+                            console.log("title "+dataFarlocchi2[film].title);
+                            filmsIncomeData.datasets.data.push(dataFarlocchi2[film].grossing);
+                            console.log("grossing "+dataFarlocchi2[film].grossing);
                         }
                         console.log('Fine parsing dati film');
                     })
