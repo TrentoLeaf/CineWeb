91db9ea578f02c5ea9bb06d1999ed3bb50c2c494
Andrea <andrea.zorzi.94@gmail.com>
2015-07-02 12:55:20 +0200

    Teo vuole che tutte le proiezioni siano ordinate. Così sia.

--- src/main/webapp/js/services/filmsPlays.js
+++ src/main/webapp/js/services/filmsPlays.js
@@ -83,6 +83,21 @@
                                 }
                             });
 
+                            // comparison by date function
+                            function compare(a,b) {
+                                var dateA = new Date(a.date);
+                                var dateB = new Date(b.date);
+
+                                if (dateA < dateB)
+                                    return -1;
+                                if (dateA > dateB)
+                                    return 1;
+                                return 0;
+                            }
+
+                            // order array by date
+                            array.sort(compare);
+
                             deferred.resolve(array);
                         },
                         function (errors) {
